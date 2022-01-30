fc97785df4c6c83a432f5a72b6332d4895d9e928
harrywangyh <harrywangyh@gmail.com>
2022-01-29 20:37:34 -0600

    parsing + checking works

--- wl.cpp
+++ wl.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <cctype>
 #include <vector>
+#include <sstream>
 using namespace std; // this is a using directive telling the compiler to check the std namespace when resolving identifiers with no prefix
 bool compareChar(char & c1, char & c2)
 {
@@ -27,7 +28,7 @@ void load_file(std::string file){
     ifstream myReadFile(file);
     if(myReadFile.fail()){
         //File does not exist code here
-        cout<< "ERROR: Invalid command\n";
+        cout<< "ERROR: Invalid command"<<endl;
     }
     // Use a while loop together with the getline() function to read the file line by line
     while (getline (myReadFile, myText)) {
@@ -38,11 +39,59 @@ void load_file(std::string file){
 }
 
 
+//this function checks which command the input invokes
+//then check if the input has the correct number of param
+//return 0 -> load 1 -> locate 2 -> new 3 -> end -1 ->bad command
+int check_command(vector<string> &niceString){
+    string terminate = "end";
+    string loader = "load";
+    string locater = "locate";
+    string reset = "new";
+    if(case_compare(niceString.at(0),loader)){
+        if(niceString.size() == 2){
+            return 0;
+        }else{
+            cout<< "ERROR: Invalid command"<<endl;
+            return -1;
+        }   
+    }
+    else if(case_compare(niceString.at(0),locater)){
+        if(niceString.size() == 3){
+            return 1;
+        }else{
+            cout<< "ERROR: Invalid command"<<endl;
+            return -1;
+        }
+    }
+    else if(case_compare(niceString.at(0),reset)){
+        if(niceString.size() == 1){
+            return 2;
+        }else{
+            cout<< "ERROR: Invalid command"<<endl;
+            return -1;
+        }
+    }
+    else if(case_compare(niceString.at(0),terminate)){
+        if(niceString.size() == 1){
+            return 3;
+        }else{
+            cout<< "ERROR: Invalid command"<<endl;
+            return -1;
+        }
+    }
+    cout<< "ERROR: Invalid command"<<endl;
+    return -1;
+}
+
 //this function parses the input string into array of strings
 void parser(std::string raw_input, vector<string> &niceString){
-    return 0;
-
-
+    //use space to separate new char, then pushback to vector
+    //stringstream ss(raw_input);
+    stringstream ss(raw_input);
+    string word;
+    while (ss >> word) {
+        niceString.push_back(word);
+    }
 }
 int main()
 {
@@ -51,11 +100,12 @@ int main()
     vector<string> niceString;
     do{
         std::cout << ">";
-        std::cin >> input;
+        getline (cin, input);
         parser(input,niceString);
+        check_command(niceString);
         //string test[3] = parser(input);
         //load_file(input);
-    }
-    while(!case_compare(input,terminate));
+        niceString.clear();
+    } while(!case_compare(input,terminate));
     return 0;
 }
